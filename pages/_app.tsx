5f5e29c311a89963c90d1a5ab7a1dfffdaeea7dd
Alex Rohleder <a.rohleder@sportradar.com>
2021-04-14 16:28:01 +0200

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import "tailwindcss/tailwind.css";
+
+function Portfolio({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default Portfolio;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+const Index = () => (
+  <main className="bg-red-100">
+    <h1>Welcome to Next.js</h1>
+  </main>
+);
+
+export default Index;
