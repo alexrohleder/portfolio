41123e8233e7125268be6c69c091a51ba6497d99
Alex Rohleder <a.rohleder@sportradar.com>
2021-04-22 15:39:32 +0200

    add go to home page link

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,9 +6,14 @@ const NotFound = () => {
       <Header />
       <hr className="h-px bg-gray-300 w-full my-12" />
       <div className="text-center">
-        <h1 className="mb-2 font-semibold text-xl">404 NOT FOUND</h1>
+        <h1 className="mb-4 font-semibold text-xl">404 NOT FOUND</h1>
         <p>
           Hey, I'm still working on this site so not all pages are finished yet.
+        </p>
+        <p>
+          <a className="text-blue-500 hover:underline" href="/">
+            Go to home page
+          </a>
         </p>
       </div>
     </main>
