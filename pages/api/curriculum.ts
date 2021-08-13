56077e875c1a22842afe2112b9703ef9da9e979c
Alex Rohleder <a.rohleder@sportradar.com>
2021-08-13 08:18:38 +0200

    generate pdf using puppeteer for better control

--- pages/api/curriculum.ts
+++ pages/api/curriculum.ts
@@ -1,27 +1,21 @@
+import puppeteer from "puppeteer";
+import absoluteUrl from "next-absolute-url";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const url = "http://alexrohleder.com";
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const pdfConvertionResp = await fetch(
-    "https://www.web2pdfconvert.com/convert/web/to/pdf?storefile=true&filename=alexrohleder-com",
-    {
-      headers: {
-        "content-type": `multipart/form-data; boundary=----WebKitFormBoundaryVSqA5YPRgIE4GTR1`,
-      },
-      referrer: "https://www.web2pdfconvert.com/",
-      body: `------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="url"\r\n\r\n${url}\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="pricing"\r\n\r\nmonthly\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="ConversionDelay"\r\n\r\n0\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="CookieConsentBlock"\r\n\r\ntrue\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="Zoom"\r\n\r\n1\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="FixedElements"\r\n\r\nabsolute\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="ViewportWidth"\r\n\r\n800\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="ViewportHeight"\r\n\r\n800\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="PageOrientation"\r\n\r\nportrait\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="PageRange"\r\n\r\n1-20\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="PageSize"\r\n\r\na4\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="MarginTop"\r\n\r\n0\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="MarginRight"\r\n\r\n0\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="MarginBottom"\r\n\r\n0\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="MarginLeft"\r\n\r\n0\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1\r\nContent-Disposition: form-data; name="ParameterPreset"\r\n\r\nCustom\r\n------WebKitFormBoundaryVSqA5YPRgIE4GTR1--\r\n`,
-      method: "POST",
-      mode: "cors",
-    }
-  );
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const { origin } = absoluteUrl(req, "http://localhost:3000");
+  const homepage = await page.goto(origin);
 
-  try {
-    const pdfConvertionBody = await pdfConvertionResp.json();
-    const downloadUrl = pdfConvertionBody.Files[0].Url;
-    res.redirect(downloadUrl);
-  } catch (error) {
+  if (homepage.ok()) {
+    const buffer = await page.pdf();
+    res.setHeader("Content-Disposition", 'attachment; filename="Alex.pdf"');
+    res.setHeader("Content-Type", "application/pdf");
+    res.end(buffer);
+  } else {
     res.end(502);
-    console.error(error);
   }
+
+  await browser.close();
 };
