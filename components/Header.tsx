2d725a9e20e32b7cce6ab23f2787b770df8824ce
Alex Rohleder <a.rohleder@sportradar.com>
2021-09-22 19:29:11 +0200

    use my gmail

--- components/Header.tsx
+++ components/Header.tsx
@@ -70,7 +70,9 @@ const Header = () => {
                 d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
               />
             </svg>
-            <a href="mailto:talkto@alexrohleder.com">talkto@alexrohleder.com</a>
+            <a href="mailto:alexrohleder96@gmail.com">
+              alexrohleder96@gmail.com
+            </a>
           </div>
           <div className="flex gap-2 items-center">
             <svg
