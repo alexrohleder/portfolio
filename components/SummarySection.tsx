dc3b932243f4b8fd866472a9d4a8e174a4128157
Alex Rohleder <a.rohleder@sportradar.com>
2021-10-02 19:19:04 +0200

    improve Summary section

--- components/SummarySection.tsx
+++ components/SummarySection.tsx
@@ -1,16 +1,43 @@
+import Image from "next/image";
 import Section from "./Section";
 
 const SummarySection = () => {
   return (
     <Section title="Summary">
-      <p className="mb-2">
-        Hey! I'm a Brazilian who is pasionate about building high-impact,
-        well-performing user experiences 🤩
-      </p>
-      <p>
-        I believe that work is a big portion of my day, so I try to make the
-        best out of it for me and my colleagues.
-      </p>
+      <div className="flex flex-col gap-2">
+        <p className="flex items-center gap-1">
+          Hey! I'm a Brazilian who is pasionate about building high-impact,
+          well-performing user experiences
+          <Image
+            src="/star-struck.png"
+            alt="Grinning Face with Star Eyes"
+            width={16}
+            height={16}
+          />
+        </p>
+        <p>
+          I believe that work is a big portion of my day, so I try to make the
+          best out of it for me and my colleagues.
+        </p>
+        <p>
+          <a
+            className="text-blue-700 hover:underline"
+            href="https://www.kitchensoap.com/2012/10/25/on-being-a-senior-engineer/"
+            target="_blank"
+          >
+            On being a senior engineer
+          </a>{" "}
+          is an article from{" "}
+          <a
+            className="text-blue-700 hover:underline"
+            href="https://www.kitchensoap.com/"
+            target="_blank"
+          >
+            Kitchen Soap
+          </a>{" "}
+          that I like and use as guidance.
+        </p>
+      </div>
     </Section>
   );
 };
