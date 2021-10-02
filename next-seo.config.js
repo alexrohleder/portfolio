ff96b11a5c0cbaa5f5965e8e328d0d596c6403ca
Alex Rohleder <a.rohleder@sportradar.com>
2021-10-03 01:09:32 +0200

    improve next-seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,28 +1,29 @@
-const description = "Software engineer with 5+ years of experience. Specialized in front-end and with a foot in every step of the development pipeline. Open for contractor jobs.";
+const description =
+  "Frontend software engineer with 5+ years of experience and pasionate about building high-impact, well-performing user experiences.";
 
 export default {
-  "title": "Alex Rohleder - Software Engineer",
-  "titleTemplate": "%s - Alex Rohleder",
+  title: "Alex Rohleder - Frontend Software Engineer",
+  titleTemplate: "%s - Alex Rohleder",
   description,
-  "openGraph": {
-    "title": "Alex Rohleder - Software Engineer",
+  openGraph: {
+    title: "Alex Rohleder - Frontend Software Engineer",
     description,
-    "type": "website",
-    "locale": "en_IE",
-    "url": "https://alexrohleder.com",
-    "site_name": "Alex Rohleder Portfolio",
-    "images": [
-      {
-        "url": "/card.png",
-        "width": 800,
-        "height": 600,
-        "alt": "Alex Rohleder Portfolio"
-      }
-    ]
+    type: "website",
+    locale: "en_IE",
+    url: "https://alexrohleder.com",
+    site_name: "Alex Rohleder Portfolio",
+    // images: [
+    //   {
+    //     url: "/card.png",
+    //     width: 800,
+    //     height: 600,
+    //     alt: "Alex Rohleder Portfolio",
+    //   },
+    // ],
   },
-  "twitter": {
-    "handle": "@alexrohleder",
-    "site": "@alexrohleder",
-    "cardType": "summary_large_image"
-  }
-}
+  twitter: {
+    handle: "@alexrohleder",
+    site: "@alexrohleder",
+    cardType: "summary_large_image",
+  },
+};
