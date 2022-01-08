572c297be2a6ab80186cdd623cebbeab42d46223
Alex Rohleder <a.rohleder@sportradar.com>
2022-01-08 20:39:36 +0100

    adds canonical link tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 import seo from "../../next-seo.config";
 import "tailwindcss/tailwind.css";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const { route } = useRouter();
+
+  const domain =
+    process.env.NODE_ENV === "production"
+      ? "https://alexrohleder.com"
+      : "http://localhost:3000";
+
+  const canonical = `${domain}${route === "/" ? "" : route}`;
+
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange>
+      <Head>
+        <link rel="canonical" href={canonical} />
+      </Head>
       <DefaultSeo {...seo} />
       <Component {...pageProps} />
     </ThemeProvider>
