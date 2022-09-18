29dc178e0908bb9db205db11ab52f14781f51af9
Alex Rohleder <alexrohleder96@gmail.com>
2022-09-18 19:49:54 +0200

    improve skill section

--- src/components/blocks/SkillBlock.tsx
+++ src/components/blocks/SkillBlock.tsx
@@ -1,29 +1,13 @@
-import { StarIcon } from "@heroicons/react/outline";
-
 type Props = {
   name: string;
-  rating: 1 | 2 | 3 | 4 | 5;
+  years: 1 | 2 | 3 | 4 | 5;
 };
 
 function SkillBlock(props: Props) {
-  const getStarClassName = (index: number) => {
-    return `h-3 w-3${
-      index <= props.rating
-        ? " fill-blue-500 dark:fill-blue-400 dark:stroke-blue-400"
-        : ""
-    }`;
-  };
-
   return (
-    <div className="print:flex items-center justify-between">
+    <div className="items-center justify-between print:flex">
       <div>{props.name}</div>
-      <div className="flex items-center">
-        <StarIcon className={getStarClassName(1)} />
-        <StarIcon className={getStarClassName(2)} />
-        <StarIcon className={getStarClassName(3)} />
-        <StarIcon className={getStarClassName(4)} />
-        <StarIcon className={getStarClassName(5)} />
-      </div>
+      <div className="flex items-center">{props.years} years</div>
     </div>
   );
 }
--- src/components/sections/TopSkillsSection.tsx
+++ src/components/sections/TopSkillsSection.tsx
@@ -6,19 +6,19 @@ function TopSkillsSection() {
     <BaseSection title="Top Skills" footerNote="Missing any? Contact me 😊">
       <div className="hidden gap-8 print:flex">
         <div className="flex-1">
-          <SkillBlock name="React" rating={5} />
-          <SkillBlock name="AWS" rating={3} />
+          <SkillBlock name="React" years={5} />
+          <SkillBlock name="AWS" years={3} />
         </div>
         <div className="flex-1">
-          <SkillBlock name="TypeScript" rating={5} />
-          <SkillBlock name="Sanity" rating={4} />
+          <SkillBlock name="TypeScript" years={5} />
+          <SkillBlock name="Sanity" years={1} />
         </div>
       </div>
       <div className="grid grid-cols-2 grid-rows-2 gap-2 print:hidden">
-        <SkillBlock name="React" rating={5} />
-        <SkillBlock name="TypeScript" rating={5} />
-        <SkillBlock name="Sanity" rating={4} />
-        <SkillBlock name="AWS" rating={3} />
+        <SkillBlock name="React" years={5} />
+        <SkillBlock name="TypeScript" years={5} />
+        <SkillBlock name="Sanity" years={1} />
+        <SkillBlock name="AWS" years={3} />
       </div>
     </BaseSection>
   );
